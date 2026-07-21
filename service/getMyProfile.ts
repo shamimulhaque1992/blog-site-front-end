7048a74c5f82a4e076a40d898b48b1b6d6d20fc4
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2026-07-21 19:49:43 +0600

    some changes

---
+++ service/getMyProfile.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getMyProfile = async () => {
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get("accessToken")?.value;
+
+  if (!accessToken) {
+    return {
+      success: false,
+      message: "User not loggedIn",
+    };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}users/me`, {
+    headers: {
+      // Authorization:`Bearer ${accessToken}`
+      // Authorization:`Bearer ${accessToken}`
+      Cookie: `accessToken=${accessToken}`,
+    },
+  });
+
+  const result = await res.json();
+  console.log("🚀 ~ getMyProfiel ~ result:", result)
+  return result;
+};
